08b1aa8c1b6b1d52f95e0bf376b19cae5b977233
rafaax <rafavfx1@gmail.com>
2024-06-18 09:17:50 -0300

    ajustando a ref do source

--- documents.sh
+++ documents.sh
@@ -1,4 +1,4 @@
-source source.bashrc
+source /c/xampp/htdocs/d4sign/source.bashrc
 
 curl --request GET \
      --url "https://sandbox.d4sign.com.br/api/v1/documents?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
