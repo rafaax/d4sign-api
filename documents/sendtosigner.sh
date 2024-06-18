f186d2cbd2650a0737cd225037d67aa14236162f
rafaax <rafavfx1@gmail.com>
2024-06-18 13:00:30 -0300

    send to signer

--- documents/sendtosigner.sh
+++ documents/sendtosigner.sh
@@ -1,10 +1,14 @@
 source /c/xampp/htdocs/d4sign/env.bashrc
 
-uuid_document=478e6cb6-2078-464e-9e48-e548073ffe9c
+uuid_document=3c345d9e-f621-4268-aead-fcdcca364228
 
 curl --request POST \
-    --url "https://sandbox.d4sign.com.br/api/v1/documents/$uuid_document/createlist?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
+    --url "https://sandbox.d4sign.com.br/api/v1/documents/$uuid_document/sendtosigner?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
     --header 'content-type: application/json' \
     --header 'accept: application/json' \
-    --data ''
+    --data '{
+        "message": "Assinatura requisitada...",
+        "skip_email": "0",
+        "workflow": "0",
+    }'
     
