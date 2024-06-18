8fa889461891cf207be7aa14b0112d9059df3be1
rafaax <rafavfx1@gmail.com>
2024-06-18 14:43:11 -0300

    cadastrar signatario

--- documents/signatarios/cadastrar_signatarios.sh
+++ documents/signatarios/cadastrar_signatarios.sh
@@ -1,6 +1,6 @@
 source /c/xampp/htdocs/d4sign/env.bashrc
 
-uuid_document=478e6cb6-2078-464e-9e48-e548073ffe9c
+uuid_document=3c345d9e-f621-4268-aead-fcdcca364228
 
 curl --location --request POST \
     --url "https://sandbox.d4sign.com.br/api/v1/documents/$uuid_document/createlist?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
@@ -9,22 +9,9 @@ curl --location --request POST \
     --data '{
         "signers": [
             {
-                "email": "raphael.meireles@vetorian.com",
-                "act": "1",
-                "foreign": "1",
-                "foreign_language": "ptBR",
-                "certificadoicpbr": "0",
-                "assinatura_presencial": "0",
-                "docauth": "0",
-                "docauthandselfie": "0",
-                "embed_methodauth": "email",
-                "upload_obs": "Contrato de Serviço"
-            },
-            {
                 "email": "rafavfx1@gmail.com",
                 "act": "1",
-                "foreign": "1",
-                "foreign_language": "ptBR",
+                "foreign": "0",
                 "certificadoicpbr": "0",
                 "assinatura_presencial": "0",
                 "docauth": "0",
