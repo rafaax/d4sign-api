8d05fab6d67b3638e3df36e97b1deaaf66f0c3ac
rafaax <rafavfx1@gmail.com>
2024-06-19 09:12:11 -0300

    post/webhooks

--- documents/webhooks/cadastrar_webhook.sh
+++ documents/webhooks/cadastrar_webhook.sh
@@ -4,7 +4,7 @@ uuid_document=3c345d9e-f621-4268-aead-fcdcca364228
 
 
 curl --request POST \
-    --location 'https://sandbox.d4sign.com.br/api/v1/documents/3c345d9e-f621-4268-aead-fcdcca364228/webhooks?tokenAPI=live_dab3a1b78888813bf1db0a1bd61f885dd90ca88790eee60cb7bedf856b4ee28e&cryptKey=live_crypt_RT6bUm2UQw4UOclKBvTAbs28JWgGzjdG' \
+    --location "https://sandbox.d4sign.com.br/api/v1/documents/$uuid_document/webhooks?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
     --header 'Content-Type: application/json' \
     --data '{
         "url": "https://vetorian.requestcatcher.com/test"
