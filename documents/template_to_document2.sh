2d60a63f68a4760a3dc11e0f1e44a7c59de940de
rafaax <rafavfx1@gmail.com>
2024-06-20 10:28:35 -0300

    template document /post 2

--- documents/template_to_document2.sh
+++ documents/template_to_document2.sh
@@ -1,23 +1,22 @@
 source /c/xampp/htdocs/d4sign/env.bashrc
-
+#
 uuid_safe=35d49dc9-10d4-4ac2-91ec-29f2b73f66b2
 uuid_template="MTgxOA=="
-
-curl --location --request POST \
+# 
+curl --request POST \
     --url "https://sandbox.d4sign.com.br/api/v1/documents/$uuid_safe/makedocumentbytemplateword?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
-    --header 'Cookie: csrf_cookie_d4sign=3879ce4ca30af3608b73482cb1e79ff5' \
     --header 'content-type: application/json' \
-    --header 'accept: application/json' \
-    --data "{
-        "name_document":  "Contrato X",
+    --data '
+    {
+        "name_document":  "Contrato exemplo",
         "templates": {
-            "$uuid_template" : {
-                "nome_empresa" : "6001",
-                "sede_empresa": "Contratado",
-                "cnpj_empresa": "Empresa Contratado",
+            "MTgxOA==" : {
+                "nome_empresa" : "Vetorian",
+                "sede_empresa": "Mogi das Cruzes",
+                "cnpj_empresa": "39.959.020/0001-20",
                 "dia_atual": "20",
                 "mes_atual": "junho",
                 "ano_atual": "2024"
             }
         }
-    }"
+    }'
