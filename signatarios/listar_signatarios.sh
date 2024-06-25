f109384adf612cf04a1e4b2b5a114ff8f72a335d
rafaax <rafavfx1@gmail.com>
2024-06-25 09:57:39 -0300

    get listar signatarios

--- signatarios/listar_signatarios.sh
+++ signatarios/listar_signatarios.sh
@@ -1,6 +1,6 @@
 source /c/xampp/htdocs/d4sign/env.bashrc
 
-uuid_document=3f04a65a-bb87-458d-85f6-5cae6fc73db8
+uuid_document=c315019c-9f78-43e0-b8bd-a0cfa2bfe0ee
 
 curl --location --request GET \
     --url "https://sandbox.d4sign.com.br/api/v1/documents/$uuid_document/list?tokenAPI=$TOKEN&cryptKey=$CRYPT_KEY" \
